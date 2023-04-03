b2b8cde6335181cf559d74490aa202726e511966
Carlos Tardez <carlostaasin@gmail.com>
2023-04-03 13:02:21 +0200

    feat: render html

--- components/podcast/card-details.tsx
+++ components/podcast/card-details.tsx
@@ -5,7 +5,7 @@ export const CardDetails: React.FC<{ podcast: PodcastDetails }> = ({ podcast }) 
     const collectionImg = podcast.results[0].artworkUrl600;
 
     return (
-        <div className="card" style={{ width: 300 }}>
+        <div className="card" style={{ width: 300, overflow: 'hidden' }}>
             <Image src={collectionImg} width={300} height={300} alt={collectionImg} />
             <div className="card-body" style={{ overflow: 'hidden' }}>
                 <p className="card-text">{podcast.results[0].collectionName}</p>
--- components/podcast/card-mp3.tsx
+++ components/podcast/card-mp3.tsx
@@ -1,11 +1,12 @@
 import { Item } from "@/interfaces/podcast/item";
+import { htmlToText } from 'html-to-text';
 
 export const CardMp3: React.FC<{ item: Item }> = ({ item }) => {
     return (
         <div className="card">
             <div className="card-body">
                 <h5 className="card-title">{item.title}</h5>
-                <p className="card-text">{item.description}</p>
+                <p className="card-text">{htmlToText(item.description[0])}</p>
             </div>
             <div className="card-body">
                 <audio src={item.enclosure[0].$.url} controls />
